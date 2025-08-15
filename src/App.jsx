5d5ccde57f5fa2bb362cb404f38bf36381927595
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-08-14 21:06:51 -0300

    Começo do Main;

--- src/App.jsx
+++ src/App.jsx
@@ -9,37 +9,40 @@ import Result from "./components/Result";
 
 // Images
 import logo from "./images/logo.svg";
+import icon_dollar from "./images/icon-dollar.svg";
 
 const tip_options = [5, 10, 15, 25, 50];
 
 function App() {
   return (
     <>
-      <main>
+      <main id="wrapper">
         <img src={logo} alt="Logo" style={{ maxWidth: "4rem" }} />
 
-        <section id="content_section">
-          <div className="info_section">
-            <InputContainer label="Bill" />
+        <div id="main_container">
+          <section id="content_section">
+            <div className="info_section">
+              <InputContainer label="Bill" img={icon_dollar} />
 
-            <div className="tip_container">
-              {tip_options.map((tip, index) => (
-                <TipContainer tip={tip} key={index} />
-              ))}
+              {/* <div className="tip_container">
+                {tip_options.map((tip, index) => (
+                  <TipContainer tip={tip} key={index} />
+                ))}
+              </div>
+
+              <InputContainer label="Number of people" /> */}
+            </div>
+          </section>
+
+          <section id="results_container">
+            <div className="amount_section">
+              <Result label="Tip Amount" />
+              <Result label="Total" />
             </div>
 
-            <InputContainer label="Number of people" />
-          </div>
-        </section>
-
-        <section id="results_container">
-          <div className="amount_section">
-            <Result label="Tip Amount" />
-            <Result label="Total" />
-          </div>
-
-          <button>RESET</button>
-        </section>
+            <button>RESET</button>
+          </section>
+        </div>
       </main>
     </>
   );
--- src/components/InputContainer.jsx
+++ src/components/InputContainer.jsx
@@ -3,7 +3,17 @@ import PropTypes from "prop-types";
 import "./InputContainer.css";
 
 function InputContainer({ label, img }) {
-  return <div>{label}</div>;
+  return (
+    <div className="input_container">
+      <label htmlFor={label}>{label}</label>
+      <div className="input_wrapper">
+        <div className="image_container">
+          <img src={img} alt="Icon" style={{ maxWidth: "2rem" }} />
+        </div>
+        <input type="number" placeholder="0" name={label} id={label} />
+      </div>
+    </div>
+  );
 }
 
 InputContainer.PropTypes = {
